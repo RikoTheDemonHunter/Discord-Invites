fb13e39124e12fe124ed3b493010a515d99093ed
90PuroXx <126303785+RikoTheDemonHunter@users.noreply.github.com>
2025-01-24 11:30:44 +0800

    Update Automation.java

--- Automation.java
+++ Automation.java
@@ -3,3 +3,37 @@ local {}
 discord server function:Null
   Remote Service Url:LocalServer:HttpServiceHost
   ServicePrompt:FirstService 
+  LocalService:function request update = local update Github File
+  do wait (1) 
+  end
+      end 
+          end
+        end)
+{}
+
+ local function obscure prompt = function ()
+   discord token = 9912659302 
+   user token = 1495201 
+   client token = 923486497578029097
+  local server do function gateway request do server wait (0.8)
+   Function service prompt: https//Gateway.request.discord.token.gg
+   https//Puro Community.gg
+   local ip = 182.132.10.90
+   Server ip = 90.-12.30.-231
+   Fake Ip = 10.10.10.10.0
+   prompt function :("Gateway-Intent prompt = https//Local-Server.random name.gg
+                     end 
+                          end
+                       end
+                             end)
+   local  end) function prompt
+   do wait (2.4)
+
+   end
+        end
+             end
+            end
+                end
+              end)
+   
+   
